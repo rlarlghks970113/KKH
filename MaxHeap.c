f4a82470fa12e550e3b10c56cb7ab3a3def36463
2016024739 <2106024739@hanyang.ac.kr>
2019-04-18 03:06:17 -0700

    haha

--- MaxHeap.c
+++ MaxHeap.c
@@ -17,62 +17,58 @@ int Find(Heap* heap, int value);
 
 //HW 7
 
-int DeleteMax(Heap* heap);
-void PrintMax(Heap* heap);
+//int DeleteMax(Heap* heap);
+//void PrintMax(Heap* heap);
 
 void main(int argc, char *argv[])
 {
-	printf("hi");
-	printf("why?");
-	printf("???");
 	FILE *fi = fopen(argv[1], "r");
-	printf("hi");
 	char cv;
-	printf("hi");
 	Heap* maxHeap;
 	int heapSize, key;
-	
-	printf("hi");
+
 	while (!feof(fi))
 	{
 		fscanf(fi, "%c", &cv);
 		switch (cv)
 		{
-		case 'n' :
+		case 'n':
 			fscanf(fi, "%d", &heapSize);
 			maxHeap = CreateHeap(heapSize);
 			break;
 		case 'i':
+			
 			fscanf(fi, "%d", &key);
 			Insert(maxHeap, key);
 			break;
 		case 'd':
-//			DeleteMax(maxHeap);
+			//			DeleteMax(maxHeap);
 			break;
 		case 'p':
-//			PrintHeap(maxHeap);
+			//			PrintHeap(maxHeap);
 			break;
 		case 'f':
 			fscanf(fi, "%d", &key);
 			if (Find(maxHeap, key))
 			{
-				printf("%d is in the tree.\n", key);
+				printf("%d is in the heap.\n", key);
 			}
 			else
 			{
-				printf("%d is not in the tree.\n", key);
+				printf("%d is not in the heap.\n", key);
 			}
 			break;
 		}
 	}
 	fclose(fi);
+
 }
 
 Heap* CreateHeap(int heapSize)
 {
 	Heap* tmpHeap = malloc(sizeof(struct HeapStruct));
 
-	tmpHeap->capacity = heapSize + 1;
+	tmpHeap->capacity = heapSize;
 	tmpHeap->size = 0;
 	tmpHeap->element = malloc(sizeof(int)* tmpHeap->capacity);
 
@@ -82,43 +78,46 @@ Heap* CreateHeap(int heapSize)
 void Insert(Heap* heap, int value)
 {
 	int i;
-
+	
 	if (heap == NULL)
 	{
 		printf("There is no heap\n");
-		return;
-	}
 
-	for (int j = 1; value <= heap->size;j++)
-	{
-		if (heap->element[j] == value)
-		{
-			printf("%d is already in the heap\n", value);
-			return;
-		}
 	}
-	
-	if (heap->capacity <= heap->size)
+	else if(Find(heap, value))
+	{
+		printf("%d is already in the heap\n", value);
+	}
+	else if (heap->capacity <= heap->size)
 	{
 		printf("Insertion Error: Max Heap is full\n");
 	}
-	else
+	else if (heap->size == 0)
 	{
-		for (i = ++heap->size; heap->element[i / 2] < value; i /= 2)
+		heap->element[++heap->size] == value;
+		printf("insert %d\n", value);
+	}
+	else 
+	{
+
+		for (i = ++heap->size; heap->element[i / 2] < value && i>1; i /= 2)
 		{
 			heap->element[i] = heap->element[i / 2];
 		}
 		heap->element[i] = value;
+		printf("insert %d\n", value);
 	}
 }
 int Find(Heap* heap, int value)
 {
-	for (int i = 1; value <= heap->size; i++)
+	
+	for (int i = 0; i < heap->size; i++)
 	{
 		if (heap->element[i] == value)
 		{
 			return 1;
 		}
 	}
+
 	return 0;
 }
